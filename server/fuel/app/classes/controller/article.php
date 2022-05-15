1a9dbdd9790e4bcc4528ca4714777ff3f3c38dd3
Uchinomaru <bravecred@gmail.com>
2022-05-16 00:44:54 +0900

    ページネーション追加

--- server/fuel/app/classes/controller/article.php
+++ server/fuel/app/classes/controller/article.php
@@ -2,15 +2,31 @@
 
 class Controller_Article extends Controller_Template
 {
+  private $per_page = 3;
+
   public function action_index()
   {
     $data = array();
+
+    // ページネーションの設定
+    $count = Model_Article::count();
+    $config = array(
+      'pagination_url' => 'article/index',
+      // article/index/1 で数字が3番目に来るから3
+      'uri_segment' => 3,
+      'num_links' => 4,
+      'per_page' => $this->per_page,
+      'total_items' => $count,
+    );
+    $pagination = Pagination::forge('article_pagination', $config);
+
     // 記事取得
     // リレーションが設定されている物はjoinして一緒に取ってくる
-    $data['articles'] = Model_Article::query()->order_by('id', 'desc')->get();
+    $data['articles'] = Model_Article::query()->order_by('id', 'desc')->limit($this->per_page)->offset($pagination->offset)->get();
 
     $this->template->title = '記事一覧';
     $this->template->content = View::forge('article/list', $data);
+    $this->template->content->set_safe('pagination', $pagination);
   }
 
   public function action_view($id = 0)
