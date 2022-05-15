ebc9966558210f93f04da33ae2b0bdf7f0a76ee7
Uchinomaru <bravecred@gmail.com>
2022-05-16 00:09:52 +0900

    記事詳細ページ

--- server/fuel/app/classes/controller/article.php
+++ server/fuel/app/classes/controller/article.php
@@ -12,4 +12,17 @@ class Controller_Article extends Controller_Template
     $this->template->title = '記事一覧';
     $this->template->content = View::forge('article/list', $data);
   }
+
+  public function action_view($id = 0)
+  {
+    $data = array();
+    // 不正なIDの場合はリダイレクト
+    // ここでのandの意味が謎
+    $id and $data['article'] = Model_Article::find($id);
+    if (!$data['article']) {
+      Response::redirect('articles');
+    }
+    $this->template->title = $data['article']->title;
+    $this->template->content = View::forge('article/view', $data);
+  }
 }
