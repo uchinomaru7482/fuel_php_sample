a3f16a5b686488c4c8955cc43d529c580217734e
Uchinomaru <bravecred@gmail.com>
2022-05-15 19:54:49 +0900

    カテゴリモデル追加

--- server/fuel/app/config/development/migrations.php
+++ server/fuel/app/config/development/migrations.php
@@ -9,6 +9,8 @@ return array(
         0 => '001_create_articles',
         1 => '002_create_users',
         2 => '003_create_comments',
+        3 => '004_create_categories',
+        4 => '005_create_article_category',
       ),
     ),
     'module' => 
