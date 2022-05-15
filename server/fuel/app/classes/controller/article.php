da9c23bc1ee749e437ce32a9d22d20850ca4709c
Uchinomaru <bravecred@gmail.com>
2022-05-15 23:02:48 +0900

    記事一覧ページ

--- server/fuel/app/classes/controller/article.php
+++ server/fuel/app/classes/controller/article.php
@@ -4,7 +4,12 @@ class Controller_Article extends Controller_Template
 {
   public function action_index()
   {
+    $data = array();
+    // 記事取得
+    // リレーションが設定されている物はjoinして一緒に取ってくる
+    $data['articles'] = Model_Article::query()->order_by('id', 'desc')->get();
+
     $this->template->title = '記事一覧';
-    $this->template->content = '';
+    $this->template->content = View::forge('article/list', $data);
   }
 }
